f863dac78d5670bac7d76df3608e81f38a1dfb0d
Szymon Zak <szymer@gmail.com>
2021-07-03 10:40:51 +0200

    data base script add

---
+++ db_creator/create_db.py
@@ -0,0 +1,76 @@
+import psycopg2
+
+USER = "postgres"
+HOST = "localhost"
+PASSWORD = "coderslab"
+DB = "communicator_db"
+sql_db_create = """
+CREATE DATABASE communicator_db;
+"""
+sql_crate_tab_users = """
+CREATE TABLE users (
+id serial primary key,
+username varchar(255),
+hashed_password varchar(80)
+);
+"""
+sql_crate_tab_msg = """
+CREATE TABLE messages (
+id serial primary key,
+from_id int NOT NULL ,
+to_id int NOT NULL , 
+creation_date timestamp default now(),
+FOREIGN KEY (from_id) REFERENCES users (id),
+FOREIGN KEY (to_id) REFERENCES users (id)
+);
+
+"""
+
+
+def create_db(sql):
+    try:
+        conn = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+        )
+        conn.autocommit = True
+
+        with conn.cursor() as cursor:
+
+            try:
+                cursor.execute(sql)
+                print("data base created")
+            except psycopg2.errors.DuplicateDatabase as err:
+                print("Duplicate Database", err)
+    except psycopg2.OperationalError as error:
+        print("Connection error", error)
+    else:
+        conn.close()
+        cursor.close()
+
+
+def create_table(sql, db):
+    conn = psycopg2.connect(
+        user=USER,
+        password=PASSWORD,
+        host=HOST,
+        database=db)
+    conn.autocommit = True
+    with conn:
+        try:
+            with conn.cursor() as cursor:
+                cursor.execute(sql)
+        except psycopg2.errors.DuplicateTable as err:
+            print('Duplicate Table', err)
+    conn.close()
+    cursor.close()
+
+
+def run():
+    create_db(sql_db_create)
+    create_table(sql_crate_tab_users, DB)
+    create_table(sql_crate_tab_msg, DB)
+
+
+run()
